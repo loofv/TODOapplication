086efc0c1c11fa6516f3a7aba33165964e1e1082
Love Hulterström <love.hulterstrom@gmail.com>
2023-10-28 00:27:42 +0200

    Saving changes

--- app/src/main/java/com/example/todo_application/data/models/ToDoTask.kt
+++ app/src/main/java/com/example/todo_application/data/models/ToDoTask.kt
@@ -1,7 +1,13 @@
 package com.example.todo_application.data.models
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity
-data class ToDoTask {
-}
+@Entity(tableName = "task_table")
+data class ToDoTask (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val description: String,
+    val priority: Priority
+)
